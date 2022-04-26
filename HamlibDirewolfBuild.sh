beeb32e44ee59fa403155f145f125d8c3f3a7766
guitarpicva <ab4mw@radcommsoft.com>
2022-04-26 14:22:40 -0400

    test

--- HamlibDirewolfBuild.sh
+++ HamlibDirewolfBuild.sh
@@ -36,6 +36,7 @@ sudo apt install build-essential git cmake libasound2-dev libudev-dev libtool do
 # so support will be compiled into direwolf
 git clone https://github.com/Hamlib/Hamlib.git
 cd ~/src/Hamlib
+# if any of these fail, the script stops
 ./bootstrap && ./configure && make -j3
 sudo make install
 sudo ldconfig
@@ -48,28 +49,53 @@ git clone https://github.com/wb2osz/direwolf.git
 git checkout dev
 cd ~/src/direwolf
 mkdir ~/src/direwolf/build && cd ~/src/direwolf/build
-cmake ..
-make -j3
+# if either of these fails, the script stops
+cmake .. && make -j3 
 sudo make install
+# create the boilerplate configuration file in the home directory
 make install-conf
+# and rename it.  ours is created below
+mv ~/direwolf.conf ~/direwolf.conf.example
 # END Direwolf install
 #############################################
-#
+# Cleanup
 #############################################
-# Start configuration into the home directory
-cd ~/src
-git clone https://github.com/guitarpicva/hfaconfig.git
-# This gives a base set of configuration files which can be
-# manipulated by configuration scripts also included in this
-# repository.  A 'git fetch' can be used to get the latest
-# and then run a specified script to update everything
-# appropriately.
-#############################################
-echo "Direwolf installation is complete.  It's time to edit the direwolf.conf file."
-echo ""
 cd ~
-echo "Use \"nano direwolf.conf\" to edit the file."
 END_TIME=$(date +%s)
 echo "It took $(($END_TIME - $START_TIME)) seconds to do this work!"
-exit 0
+wait 1
+echo "Direwolf installation is complete.  It's time to build the direwolf.conf file."
+echo "Creating Direwolf configuration file."
+echo "First, let's gather the station call sign."
+echo ""
+read -p "Enter this station's VOICE call sign: " mycall
+if [[ $mycall =~ [A-Za-z0-9]{5,6} ]]
+then
+# makei it upper case
+mycall=${mycall^^}
+fi
+# it's a fair bet that the F8101 is the only sound card
+# connected and the only one with this signature
+$sounders=$( aplay -l|grep 'USB Audio CODEC' )
+sounders=${sounders:5:1}
 
+serial=$( ls /dev/serial/by-id|grep '_B-if00' )
+cd ~
+ln -s /dev/serial/by-id/$serial ~/modem
+
+echo "ADEVICE  plughw:$sounders,0" > ~/direwolf.conf
+echo "CHANNEL 0" >> ~/direwolf.conf
+echo "MYCALL $mycall" >> ~/direwolf.conf
+echo "MODEM 300" >> ~/direwolf.conf
+# "modem" corresponds to a symlink in the home
+# folder which points to the proper device from
+# /dev/serial/by-id
+echo "PTT RIG 3086 modem" >> ~/direwolf.conf
+# a short TXDELAY of 30 ms
+echo "TXDELAY 3" >> ~/direwolf.conf
+# turn off the AGWPORT
+echo "AGWPORT 0" >> ~/direwolf.conf
+# set the port number for KISS
+echo "KISSPORT 8001" >> ~/direwolf.conf
+# the end
+exit 0
---
+++ SetupDirewolf.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo "First, let's gather the station call sign."
+echo ""
+read -p "Enter this station's VOICE call sign: " mycall
+if [[ $mycall =~ [A-Za-z0-9]{5,6} ]]
+then
+# makei it upper case
+mycall=${mycall^^}
+fi
+# it's a fair bet that the F8101 is the only sound card
+# connected and the only one with this signature
+$sounders=$( aplay -l|grep 'USB Audio CODEC' )
+sounders=${sounders:5:1}
+
+serial=$( ls /dev/serial/by-id|grep '_B-if00' )
+cd ~
+ln -s /dev/serial/by-id/$serial ~/modem
+
+echo "ADEVICE  plughw:$sounders,0" > ~/direwolf.conf
+echo "CHANNEL 0" >> ~/direwolf.conf
+echo "MYCALL $mycall" >> ~/direwolf.conf
+echo "MODEM 300" >> ~/direwolf.conf
+# "modem" corresponds to a symlink in the home
+# folder which points to the proper device from
+# /dev/serial/by-id
+echo "PTT RIG 3086 modem" >> ~/direwolf.conf
+# a short TXDELAY of 30 ms
+echo "TXDELAY 3" >> ~/direwolf.conf
+# turn off the AGWPORT
+echo "AGWPORT 0" >> ~/direwolf.conf
+# set the port number for KISS
+echo "KISSPORT 8001" >> ~/direwolf.conf
+exit 0
