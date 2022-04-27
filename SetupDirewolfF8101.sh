290c6f38a3e4159b145dbcd1c9bcd42f8a5f2584
guitarpicva <ab4mw@radcommsoft.com>
2022-04-27 16:51:40 -0400

    rename "modem" to "radio" for Hamlib serial port

--- SetupDirewolfF8101.sh
+++ SetupDirewolfF8101.sh
@@ -6,10 +6,12 @@ then
     read -p "Enter this station's VOICE call sign: " mycall
     if [[ $mycall =~ [A-Za-z0-9]{5,6} ]]
     then
-    # make it upper case
-    mycall=${mycall^^}
     fi
+else
+    mycall=$1
 fi
+# make it upper case
+mycall=${mycall^^}
 # it's a fair bet that the F8101 is the only sound card
 # connected and the only one with this signature
 sounders=$( aplay -l|grep 'USB Audio CODEC' )
@@ -17,16 +19,16 @@ sounders=${sounders:5:1}
 
 serial=$( ls /dev/serial/by-id|grep _B-if00 )
 cd ~
-ln -s /dev/serial/by-id/$serial ~/modem
+ln -s "/dev/serial/by-id/$serial" ~/radio
 
 echo "ADEVICE  plughw:$sounders,0" > ~/direwolf.conf
 echo "CHANNEL 0" >> ~/direwolf.conf
 echo "MYCALL $mycall" >> ~/direwolf.conf
-echo "MODEM 300" >> ~/direwolf.conf
-# "modem" corresponds to a symlink in the home
+echo "MODEM 300 1600:1800 3@20" >> ~/direwolf.conf
+# "radio" corresponds to a symlink in the home
 # folder which points to the proper device from
 # /dev/serial/by-id
-echo "PTT RIG 3086 ~/modem" >> ~/direwolf.conf
+echo "PTT RIG 3086 radio" >> ~/direwolf.conf
 # a short TXDELAY of 30 ms
 echo "TXDELAY 3" >> ~/direwolf.conf
 # turn off the AGWPORT
