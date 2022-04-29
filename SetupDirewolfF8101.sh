dca458c1adf1c555aed66d53b77fc0331a6d0249
guitarpicva <ab4mw@radcommsoft.com>
2022-04-29 13:08:54 -0400

    asdf

--- SetupDirewolfF8101.sh
+++ SetupDirewolfF8101.sh
@@ -40,5 +40,5 @@ echo "KISSPORT 8001" >> ~/direwolf.conf
 sed -i 's/LOGFILE=\/var\/tmp\/dw-start.log/LOGFILE=~\/dwstart.log/g' ~/dw-start.sh
 # set to CLI mode only
 sed -i 's/RUNMODE=AUTO/RUNMODE=CLI/g' ~/dw-start.sh
-sed -i 's/DIREWOLF=direwolf/DIREWOLF=\/usr\/local\/bin\/direwolf/g' ~/dw-start.sh
-sed -i 's/DWCMD=$DIREWOLF/DWCMD=$DIREWOLF -I 1/g' ~/dw-start.sh
+sed -i 's/DIREWOLF=\"direwolf\"/DIREWOLF=\"\/usr\/local\/bin\/direwolf\"/g' ~/dw-start.sh
+sed -i 's/DWCMD=\"$DIREWOLF -a 100\"/DWCMD=\"$DIREWOLF -I 1\"/g' ~/dw-start.sh
