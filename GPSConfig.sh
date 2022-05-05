6704b488c2ede5c429b873b068e8df3412279651
guitarpicva <ab4mw@radcommsoft.com>
2022-05-05 11:50:59 -0400

    fix order of ops for USBGPSConfig.sh

--- GPSConfig.sh
+++ GPSConfig.sh
@@ -3,14 +3,15 @@
 # and to feed position data to Direwolf
 # It also configures chrony which seeks NTP sources first but reverts
 # to GPS source ("NMEA" in "chronyc sources -v") when network is unavailable
-## This line needs to be added to the /etc/chrony/chrony.conf at the end
-#refclock SHM 0 offset 0.5 delay 0.2 refid NMEA
-echo "refclock SHM 0 offset 0.5 delay 0.2 refid NMEA" >> /etc/chrony/chrony.conf
+
 #
 # Also need to update /etc/default/gpsd
 #############
 sudo apt update
 sudo apt install -y chrony gpsd libgps-dev 
+## This line needs to be added to the /etc/chrony/chrony.conf at the end
+#refclock SHM 0 offset 0.5 delay 0.2 refid NMEA
+echo "refclock SHM 0 offset 0.5 delay 0.2 refid NMEA" >> /etc/chrony/chrony.conf
 # The gpsd-clients package is not really needed and avoids a lot of UI 
 # stuff and python3 which are large and doesn't work on a headlyess Pi
 # but it can safely be installed manually if the need arises for diagnostics
