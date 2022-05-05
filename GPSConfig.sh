0794d986325b6df3843b275323f976cd4ec92442
guitarpicva <ab4mw@radcommsoft.com>
2022-05-05 10:36:38 -0400

    move package install to this script and remove Direwolf config line since not doing APRS

--- GPSConfig.sh
+++ GPSConfig.sh
@@ -9,6 +9,13 @@ echo "refclock SHM 0 offset 0.5 delay 0.2 refid NMEA" >> /etc/chrony/chrony.conf
 #
 # Also need to update /etc/default/gpsd
 #############
+sudo apt update
+sudo apt install -y chrony gpsd libgps-dev 
+# The gpsd-clients package is not really needed and avoids a lot of UI 
+# stuff and python3 which are large and doesn't work on a headlyess Pi
+# but it can safely be installed manually if the need arises for diagnostics
+# regarding to the GPS receiver
+
 # Usually the GPS USB device is /dev/ttyACM0 once plugged in
 # NOTE: This will kill the existing gpsd file, so let's make a copy 
 sudo cp /etc/default/gpsd /etc/default/gpsd.orig
@@ -22,4 +29,6 @@ sudo systemctl restart gpsd
 sudo systemctl restart chrony
 #
 # Direwolf needs a line to gather the position information
-echo "GPSD localhost" >> ~/direwolf.conf
+#echo "GPSD localhost" >> ~/direwolf.conf
+# The config for Direwolf is also not really needed, since we are not
+# doing APRS.
