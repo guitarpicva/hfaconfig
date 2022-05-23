9ee65f8184814d062373e4439abc5c1687746ab0
guitarpicva <ab4mw@radcommsoft.com>
2022-05-23 10:51:05 -0400

    add the sed for UIConnect.desktop

--- HardwareModemBuild.sh
+++ HardwareModemBuild.sh
@@ -26,6 +26,7 @@ mkdir -p ~/UIConnect
 cp -f ~/hfaconfig/UIConnect ~/UIConnect/
 chmod +x ~/UIConnect/UIConnect
 cp -f ~/hfaconfig/UIConnect.desktop ~/Desktop
+sed -i "s|XXXXX|$HOME|g" ~/UICOnnect/UIConnect.desktop
 echo "UI Connect installation is complete."
 # END THIS CAN BE CHANGED....
 
